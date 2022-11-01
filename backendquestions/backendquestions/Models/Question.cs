8fe211f19858b464149443ae3390f90e2181633e
Luca Bulles <lucabulles@gmail.com>
2022-11-01 12:20:27 +0100

    changed Question model and QuestionRepository Update method to match new updated Question Model

--- backendquestions/backendquestions/Models/Question.cs
+++ backendquestions/backendquestions/Models/Question.cs
@@ -3,14 +3,20 @@
     public class Question
     {
         public Guid Id { get; set; }
-        public string Content { get; set; } = String.Empty;
-        public DateTime AddedOn { get; set; }
-        public Guid UserId { get; set; }
-        public string FileName { get; set; } = String.Empty;
-        public int NumberOfAnswers { get; set; }
-        public string Deleted { get; set; } = String.Empty;
-        public string LinkedInterest { get; set; } = String.Empty;
-        public string Language { get; set; } = String.Empty;
+        public List<Topic> Topics { get; set; } = new List<Topic>();//Topics instead of Topic for better overview
+        public string Title { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public Guid OwnerId { get; set; }
+        public List<Answer> Answers { get; set; } = new List<Answer>();//Answers instead of Answer for better overview
+        public DateTime DateOfAdded { get; set; }
+        public bool Reported { get; set; }
+        public int AmountOfLikes { get; set; }
+
+        //public string FileName { get; set; } = String.Empty;
+        //public int NumberOfAnswers { get; set; }
+        //public string Deleted { get; set; } = String.Empty;
+        //public string LinkedInterest { get; set; } = String.Empty;
+        //public string Language { get; set; } = String.Empty;
 
     }
 }
