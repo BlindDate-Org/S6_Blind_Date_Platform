b94f7b58837f7efe5f69a631b096ec6e92aebb90
Luca Bulles <lucabulles@gmail.com>
2022-11-01 12:27:14 +0100

    removed outcommented code

--- backendquestions/backendquestions/Models/Question.cs
+++ backendquestions/backendquestions/Models/Question.cs
@@ -12,11 +12,5 @@
         public bool Reported { get; set; }
         public int AmountOfLikes { get; set; }
 
-        //public string FileName { get; set; } = String.Empty;
-        //public int NumberOfAnswers { get; set; }
-        //public string Deleted { get; set; } = String.Empty;
-        //public string LinkedInterest { get; set; } = String.Empty;
-        //public string Language { get; set; } = String.Empty;
-
     }
 }
