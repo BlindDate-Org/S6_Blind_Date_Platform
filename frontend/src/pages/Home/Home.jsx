562128e8df953d9a20aa84592f2d4bed59ae08ca
Gijs Rongen <rongen.gijs@gmail.com>
2022-11-01 14:42:41 +0100

    fixed tailwind issue & added two buttons homepage

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <button class="bg-pink-400">Ask a question</button>
+        <button class="bg-blue-500">Answer a question</button>
+    </div>
+  )
+}
+
+export default Home
