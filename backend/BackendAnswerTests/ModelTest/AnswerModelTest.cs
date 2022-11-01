f53d0c85ebce1baac12cc1ff242e649172b0ee84
i428672 <428672@student.fontys.nl>
2022-11-01 10:51:35 +0100

    Added answer constructor n test for answer object
    Added answer constructor n test for answer object

---
+++ backend/BackendAnswerTests/ModelTest/AnswerModelTest.cs
@@ -0,0 +1,43 @@
+﻿using AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using backend.Models;
+
+namespace BackendAnswerTests.ModelTest
+{
+    public class AnswerModelTest
+    {
+
+        private readonly IFixture ifixture = new Fixture();
+
+        [Fact]
+        public void ConstructorTest()
+        {
+            //Arrange
+            var id = ifixture.Create<Guid>();
+            var questionId = ifixture.Create<Guid>();
+
+            Answer answer = new Answer(id, questionId, "In summer dont wear heavy clothes",
+               new DateTime(2010, 3, 11), false);
+
+            //Assert
+
+            Assert.Equal(id, answer.Id);
+            Assert.Equal(questionId, answer.QuestionId);
+            Assert.Equal("In summer dont wear heavy clothes", answer.Content);
+            Assert.Equal(new DateTime(2010, 3, 11), answer.CreatedAt);
+            Assert.False(answer.Reported);
+
+
+
+
+
+
+
+        }
+    }
+}
--- backend/backend/Models/Answer.cs
+++ backend/backend/Models/Answer.cs
@@ -2,6 +2,19 @@
 {
     public class Answer
     {
+        //Mary added constructor to initialize answer needed for testing the object
+        public Answer(Guid id, Guid questionId, string? content, DateTime createdAt, bool reported)
+        {
+            Id = id;
+            QuestionId = questionId;
+            Content = content;
+            CreatedAt = createdAt;
+            Reported = reported;
+        }
+
+       
+
+
         public Guid Id { get; set; }
         public Guid QuestionId{ get; set; }
         public string? Content { get; set; }
