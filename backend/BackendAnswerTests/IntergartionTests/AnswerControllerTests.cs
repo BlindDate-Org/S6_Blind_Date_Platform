e9f8bb5a5d46dc57e8ff6cfbe543bc66266853c1
i428672 <428672@student.fontys.nl>
2022-11-01 12:23:14 +0100

    GetAnswer by Id Method test added
    GetAnswer by Id Method test added

---
+++ backend/BackendAnswerTests/IntergartionTests/AnswerControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using AutoFixture;
+using backend.Controllers;
+using backend.Interfaces;
+using backend.Models;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BackendAnswerTests.IntergartionTests
+{
+    public class AnswerControllerTests
+    {
+        //Declare the vars that i need for tests
+        private readonly IFixture ifixture;
+        private readonly Mock<IAnswerService> answerserviceMock;//the service Mock..mocking the service
+        private readonly AnswerController systemUnderTest;
+
+        //Constructor
+        public AnswerControllerTests()
+        {
+            ifixture = new Fixture();
+            answerserviceMock = ifixture.Freeze<Mock<IAnswerService>>();
+            systemUnderTest = new AnswerController(answerserviceMock.Object);
+        }
+
+        //Test 1 Get ANswerBy Id
+
+        //test 2 getby id
+
+        [Fact]
+        public async Task GetAnswerById_ThrowsNotFound_WhenAnswerFound()
+        {
+            //Scenarios
+
+            //Arrange
+            var questionsDataMock = ifixture.Create<Answer>();//creating a answer by ifixture
+            var id = ifixture.Create<Guid>();
+
+            //Act
+            var result = await systemUnderTest.Get(id).ConfigureAwait(false);
+
+            //Assert
+            //Assert.NotNull(result);
+            result.Should().NotBeNull();
+            answerserviceMock.Verify(x => x.GetAnswerById(id), Times.Once());
+
+        }
+    }
+}
