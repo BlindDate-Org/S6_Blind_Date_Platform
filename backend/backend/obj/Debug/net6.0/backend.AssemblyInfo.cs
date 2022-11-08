7c3af0f623270bf2e014474d83627e169385832a
Mark Goertz <434548@student.fontys.nl>
2022-11-08 11:47:00 +0100

    Continuous deployment pipeline

--- backend/backend/obj/Debug/net6.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net6.0/backend.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("d44d9137-d3ac-4732-a2d2-100849e0cb42")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
